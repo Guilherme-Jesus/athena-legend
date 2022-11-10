6f47d3728aaa48d48dc6538959fa54cf2b0bb101
ed1nh0 <edsonsjr@gmail.com>
2022-11-10 10:24:03 -0300

    header vem primeiro

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,13 @@ import Blocks from './components/Blocks'
 const App: React.FC = (): React.ReactElement => {
   return (
     <div className="App">
+      <header>HEADER</header>
+
       <nav className="nav">
         <a href="#">link 1</a>
         <a href="#">link 2</a>
         <a href="#">link 3</a>
       </nav>
-
-      <header>HEADER</header>
 
       <Blocks />
 
