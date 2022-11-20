3f6521919d5434e59f74cf7aec3d053abf0c22f0
ed1nh0 <edsonsjr@gmail.com>
2022-11-20 15:28:25 -0300

    Brigando com o deepsource de novo (últma vez)

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,82 +1,82 @@
 type SensorData = {
-  atmosphericPressure: number;
-  rain: number;
-  relativeHumidity: number;
-  solarIrradiation: number;
-  temperature: number;
-  windSpeed: number;
-};
+  atmosphericPressure: number
+  rain: number
+  relativeHumidity: number
+  solarIrradiation: number
+  temperature: number
+  windSpeed: number
+}
 
 // BLOCKS
 export type IListBlocks = {
-  abrv: string;
-  blockId: string;
-  blockParent: string;
-  data: SensorData;
-  date: Date;
-  leafParent: boolean;
-  name: string;
-};
+  abrv: string
+  blockId: string
+  blockParent: string
+  data: SensorData
+  date: Date
+  leafParent: boolean
+  name: string
+}
 
 export type IListBlocksLeaf = IListBlocks & {
-  bounds: number[];
-  centroid: number[];
-};
+  bounds: number[]
+  centroid: number[]
+}
 
 // TIMELINE
 type IPastAndPresent = {
-  atmosphericPressure: number;
-  date: Date;
-  rain: number;
-  relativeHumidity: number;
-  solarIrradiation: number;
-  temperatureAverage: number;
-  temperatureMax: number;
-  temperatureMin: number;
-  windSpeed: number;
-};
+  atmosphericPressure: number
+  date: Date
+  rain: number
+  relativeHumidity: number
+  solarIrradiation: number
+  temperatureAverage: number
+  temperatureMax: number
+  temperatureMin: number
+  windSpeed: number
+}
 
 type IFuture = {
-  date: Date;
-  rain: number;
-  rainPrediction: string;
-  rainProbability: number;
-  temperatureMax: number;
-  temperatureMin: number;
-};
+  date: Date
+  rain: number
+  rainPrediction: string
+  rainProbability: number
+  temperatureMax: number
+  temperatureMin: number
+}
 
 export type IForecast = {
-  blockId?: string;
-  forecast?: IFuture[];
-  name: string;
-  past?: IPastAndPresent[];
-  present?: IPastAndPresent;
-};
+  blockId?: string
+  forecast?: IFuture[]
+  name: string
+  past?: IPastAndPresent[]
+  present?: IPastAndPresent
+}
 
 type ILineAlerts = {
-  finish: Date;
-  info: string;
-  level: number;
-  start: Date;
-  type: string;
-};
+  finish: Date
+  info: string
+  level: number
+  start: Date
+  type: string
+}
 
 export type ILine = IPastAndPresent &
   IFuture & {
-    alerts: ILineAlerts[] | null;
-  };
+    alerts: ILineAlerts[] | null
+  }
 
 export type ITimeline = {
-  blockId: string;
-  line: ILine[];
-  name: string;
-};
+  blockId: string
+  line: ILine[]
+  name: string
+}
 
 // USERS
 export type IUsers = {
-  displayName: string;
-  email: string;
-  id: string;
-  phone: string;
-  photoURL: string;
-};
+  displayName: string
+  email: string
+  id: string
+  phone: string
+  photoURL: string
+}
